aae71f688921110c69328ed958eb97348a7df5a6
zhongwu <zhongwuzw@qq.com>
2016-08-07 21:52:30 +0800

    add readme.md
    add readme.md

--- ZhihuDaily/HomePageViewController.m
+++ ZhihuDaily/HomePageViewController.m
@@ -130,7 +130,6 @@ static const CGFloat TestViewControllerHeadScrollHeight = 190.0f;
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
     if (section == 0) {
         return CGFLOAT_MIN;
-//        return 0.1;
     }
     return TABLE_HEADER_VIEW_HEIGHT;
 }
@@ -199,15 +198,5 @@ static const CGFloat TestViewControllerHeadScrollHeight = 190.0f;
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
